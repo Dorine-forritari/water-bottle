9b382b3a644a0dab7e25aa96da21b5f19668402d
Dorine van Belzen <dorinevanbelzen@gmail.com>
2021-10-21 18:22:20 +0000

    defined class

--- water_bottle.py
+++ water_bottle.py
@@ -1,2 +1,19 @@
+"""Write a class `WaterBottle` which has max capacity (parameter should be called max_capacity, with default 2L) and volume (member variable should be called volume, and initializes as 0L).
+It has 3 methods (excluding __init__)
+1. __str__() : returns a string representing how many liters are in the bottle (print to 1 decimal places, e.g. "2.3L").
+2. fill() : Fills the bottle to max capacity
+3. drink(amount) : drinks a certain amount from the bottle
+* if the amount is less than 0, nothing changes
+* if the amount is more than the current volume, volume becomes 0
+* else the amount is subtracted from the volume"""
+
+
 class WaterBottle:
-    pass
+    def __init__(self, max_capacity=2, volume=0):
+        self.max_capacity = max_capacity
+        self.volume = volume
+
+
+water_bottle1 = WaterBottle(3, 2)
+print(water_bottle1.max_capacity)
+print(water_bottle1.volume)
